1090a1e001583f2849c98b894863db94a356fbf4
I'm having a bad day . <pranupong.49@gmail.com>
2026-02-01 15:30:00 +0700

    101

--- project/main.java
+++ project/main.java
@@ -1,7 +1,9 @@
 package project;
-
+import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        System.out.println(i * 2);
     }
 }
